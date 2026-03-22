0eb5829c35e4a7e72c2a17c2b1bd81adf87511f1
mainamuchiru <mainaephantus@gmail.com>
2026-03-23 01:28:46 +0300

    Garden management loops added

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,30 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+function checktemp() {
+  if (temperature > 80) {
+    console.log("Watering on");
+  } else {
+    console.log("Watering off");
+  }
+}
+function lightscontrol() {
+  if (timeOfDay == "evening" || timeOfDay == "night") {
+    console.log("Lights on");
+  } else {
+    console.log("Lights off");
+  }
+}
+
+function soilmoisture(){
+    while (soilMoisture < 40) {
+        soilMoisture += 5
+        console.log(40)
+    }
+}
+
+checktemp();
+lightscontrol();
+soilmoisture();
+
